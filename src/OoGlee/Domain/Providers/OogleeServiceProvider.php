e4c78d936b8c555bcca59f537f85a7d88690cbdb
Rowland Mtetezi <rowlandmtetezi@gmail.com>
2015-08-18 09:03:17 +0300

    fix on providers

--- src/OoGlee/Domain/Providers/OogleeServiceProvider.php
+++ src/OoGlee/Domain/Providers/OogleeServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class OogleServiceProvider extends ServiceProvider {
+class OogleeServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
