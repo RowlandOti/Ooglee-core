c3a036d1b4c67ac2a1f701cce0fd8feef14eff23
RowlandOti <rowlandotienoo@gmail.com>
2016-04-21 09:56:55 +0300

    fix: rectify path of abstract Controller

--- src/OoGlee/Framework/Http/Controllers/Controller.php
+++ src/OoGlee/Framework/Http/Controllers/Controller.php
@@ -1,4 +1,4 @@
-<?php namespace Ooglee\Framework\Http\Controllers\Controllers;
+<?php namespace Ooglee\Framework\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
